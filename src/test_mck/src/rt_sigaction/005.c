22c826feadabb8fd2da64519a335068b0223f7ae
Ken Sato <ken.sato.ty@hitachi-solutions.com>
2018-03-20 10:02:35 +0900

    sigaction: adapt to behavior of Linux.

--- src/test_mck/src/rt_sigaction/005.c
+++ src/test_mck/src/rt_sigaction/005.c
@@ -7,13 +7,15 @@ SETUP_ALIAS(TEST_SUITE, TEST_NUMBER, DELEGATE_NUMBER)
 
 RUN_FUNC(TEST_SUITE, TEST_NUMBER)
 {
+	struct sigaction act;
 	int i;
 	int result;
 
 	int err_sig[4] = {-1, 65, SIGKILL, SIGSTOP};
 
+	act.sa_handler = SIG_DFL;
 	for(i = 0; i < (sizeof(err_sig)/sizeof(err_sig[0])); i++) {
-		result = sigaction(err_sig[i], NULL, NULL);
+		result = sigaction(err_sig[i], &act, NULL);
 		printf("sigaction(%d) = %d (errno=%d)\n", err_sig[i], result, errno);
 		tp_assert(result == -1, "ERROR:sigaction return success.");
 		tp_assert(errno == EINVAL, "ERROR:sigaction return other error number.");
